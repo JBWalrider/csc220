0c59334b6f2c6da6f8b457f392c0ba54c6392c94
jbwalrider <jan.lin.miami@gmail.com>
2018-08-29 16:23:32 -0400

    lab 2

---
+++ Lab01/src/lab01/CoinFlipExperiment1.java
@@ -0,0 +1,14 @@
+package lab01;
+
+public class CoinFlipExperiment1 {
+	
+	public static void main(String[] args) {
+		System.out.println(coinFlipExperiment());
+	}
+	
+	public static int coinFlipExperiment() {
+        int i = 1;
+        return ++i;
+	}
+	
+}
