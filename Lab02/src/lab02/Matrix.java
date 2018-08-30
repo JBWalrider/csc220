9e61a37354440b9b8dcc01d3d74858219dd6fd5e
jbwalrider <jan.lin.miami@gmail.com>
2018-08-30 13:58:34 -0400

    nig#

--- Lab02/src/lab02/Matrix.java
+++ Lab02/src/lab02/Matrix.java
@@ -26,7 +26,7 @@ public class Matrix {
 	{
 		numRows = d.length;
 		numColumns = d[0].length;
-		data = new int[numRows][numColumns];
+    	data = new int[numRows][numColumns];
 		for (int i = 0; i < numRows; i++) {
 			for (int j = 0; j < numColumns; j++) {
 				data[i][j] = d[i][j];
@@ -54,33 +54,51 @@ public class Matrix {
 		if(!(o instanceof Matrix)) // make sure the Object we're comparing to is a Matrix
 			return false;
 		Matrix m = (Matrix)o; // if the above was not true, we know it's safe to treat 'o' as a Matrix
-		
-		/*
-		 * TODO: replace the below return statement with the correct code, you must return the correct value
-		 * 			after determining if this matrix is equal to the input matrix
-		 */
-		return false; // placeholder
+	    if (m.data.length == data.length && m.data[0].length == data[0].length) {
+            for (int i = 0; i < numRows; i++) {
+                for (int j = 0; j < numColumns; j++) {
+                    if (m.data[i][j] != data[i][j]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        else return false;
+		return true; // placeholder
 	}
 
 	public Matrix times(Matrix m)
 	{
-		/*
-		 * TODO: replace the below return statement with the correct code, 
-		 *  This function must check if the two matrices are compatible for multiplication, if not, return null.
-		 *  If they are compatible, determine the dimensions of the resulting matrix, and fill it in with
-		 *  the correct values for matrix multiplication
-		 */
-		return null; // placeholder
-	}
+		
+        Matrix n = new Matrix(numRows,m.numColumns);
+		if (numColumns == m.numRows) {
+            int t = 0;
+            for (int i = 0; i < n.numRows; i++) {
+                for (int j = 0; j < n.numColumns; j++) {
+                    t = 0;
+                    for (int k = 0; k < numColumns; k++) {
+                        t += data[i][k]*m.data[k][j];
+                    }
+                    n.data[i][j] = t;
+                }
+            }
+        }
+        else return null;
+        return n;
+    }	
+
 	
 	public Matrix plus(Matrix m)
 	{
-		/*
-		 * TODO: replace the below return statement with the correct code, 
-		 *  This function must check if the two matrices are compatible for addition, if not, return null.
-		 *  If they are compatible, create a new matrix and fill it in with
-		 *  the correct values for matrix addition
-		 */
-		return null; // placeholder
+        Matrix n = new Matrix(data);
+		if (m.data.length == n.data.length && m.data[0].length == n.data[0].length) {
+            for (int i = 0; i < numRows; i++) {
+                for (int j = 0; j < numColumns; j++) {
+                    n.data[i][j] += m.data[i][j];
+                }
+            }
+        }
+        else return null;
+        return n;
 	}	
 }
--- Lab02/src/lab02/MatrixTester.java
+++ Lab02/src/lab02/MatrixTester.java
@@ -5,32 +5,44 @@
 package lab02;
 
 public class MatrixTester {
-	public static void main(String[] args)
-	{	
-		//Experiment 1
-		//Matrix M1 = new Matrix(2, 3);
-		
-		//Experiment 2 - do not uncomment before you finish the steps required
-		//Matrix M1 = new Matrix();
-		
-		//Experiment 3
-		/*
-		Matrix M1 = new Matrix(new int[][]
-                {{1, 2, 3},
-				  {2, 5, 6}});		
-		
-		*/
-		Matrix M2 = new Matrix(new int[][]
-		                                 {{4, 5},
-										  {3, 2},
-										  {1, 1}});
-		
-		/* 
-		 * Note that the print will not be correct until you have implemented the toString method.
-		 * This is just one example of a test of making sure your constructor is working
-		 * You should write more test and experiment with various constructor cases
-		 */
-		
-		System.out.println("M1 is: " + M2 + "done.");		
-	}
+		public static void main(String[] args)
+		{			
+			Matrix M1 = new Matrix(new int[][]
+			                                 {{1, 2, 3},
+											  {4, 5, 6}});
+			
+			Matrix M2 = new Matrix(new int[][]
+			                                 {{7, 8, 9},
+											  {10, 11, 12}});
+			
+			Matrix M3 = new Matrix(new int[][] {{7, 8},
+												{9, 10}, 
+												{11, 12}});
+			
+			/* 
+			 * Note that none of the tests below will be correct until you have implemented all methods.
+			 * This is just one example of a test, you must write more tests and cover all cases.
+			 */			
+			
+			Matrix ComputedAddition = M1.plus(M2);
+			
+			System.out.println("Computed result for M1 + M2:\n" + ComputedAddition);
+						
+			// this is the known correct result of multiplying M1 by M2
+			Matrix referenceMultiply = new Matrix(new int[][]
+			                                                {{58, 64},
+															 {139, 154}});			
+			
+			// get the matrix computed by your times method
+			Matrix computedMultiply = M1.times(M3);
+			
+			// exercises your toString method
+			System.out.println("Computed result for M1 * M3:\n" + computedMultiply); 
+			
+			// exercises your .equals method, and makes sure that your computed result is the same as the known correct result
+			if(!referenceMultiply.equals(computedMultiply)) 
+				System.out.println("Your multiplication is wrong. Should be:\n" + referenceMultiply);
+			
+			//TODO: fill in more tests that fully exercise all of your methods
+		}		
 }
